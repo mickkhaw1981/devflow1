42023f892f499b3a8488104f65e3932073149813
Mike Khaw <mikekhawnow@gmail.com>
2024-11-30 21:32:11 -0800

    Enhance ESLint configuration and update dependencies; modify homepage layout
    - Updated .eslintrc.json to extend additional configurations: standard, tailwindcss, and prettier.
    - Added import plugin and defined import/order rules for better module organization.
    - Updated package.json and package-lock.json to include new ESLint and Prettier dependencies.
    - Refactored homepage layout in page.tsx for improved styling and structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-black">Hello World</h1>
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
   );
 }
