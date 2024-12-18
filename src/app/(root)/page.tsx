c4ff5c1aaa136e7eb3ba5e8ef0261c08c3592c18
Mike Khaw <mikekhawnow@gmail.com>
2024-12-18 11:01:42 -0800

    Refactor Home component to include log out functionality

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,27 @@
-const Home = () => (
-  <main className="flex min-h-screen flex-col items-center justify-center gap-8">
-    <h1 className="font-space-grotesk text-8xl font-bold">Hello world</h1>
-    <h1 className="text-8xl font-bold">Hello world</h1>
-  </main>
-);
+import { Button } from "@/components/ui/button";
+import { auth, signOut } from "~/auth";
+import ROUTES from "~/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-8">
+      <h1 className="font-space-grotesk text-8xl font-bold">Hello world</h1>
+      <h1 className="text-8xl font-bold">Hello world</h1>
+      {/* add a form with a button for log out */}
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </main>
+  );
+};
 
 export default Home;
