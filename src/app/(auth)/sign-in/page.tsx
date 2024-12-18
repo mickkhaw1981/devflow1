ee92f107cb2e1d0cba8751b7a116dc687f2b8ac1
Mike Khaw <mikekhawnow@gmail.com>
2024-12-18 14:44:53 -0800

    Update authentication forms to use reusable AuthForm component

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 import React from "react";
 
 export default function SignInPage() {
-  return <div>Sign In</div>;
+  return (
+    <AuthForm
+      formType="SIGN_IN"
+      schema={SignInSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 }
