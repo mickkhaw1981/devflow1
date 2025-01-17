af28683689e9614c9099b4bef81e119c80467a63
Mike Khaw <mikekhawnow@gmail.com>
2025-01-16 16:43:47 -0800

    Implemented Question Form UI on Ask Question page

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import QuestionForm from "@/components/forms/QuestionForm";
 
 const AskQuestion = () => {
-  return <div>Ask a Question</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
