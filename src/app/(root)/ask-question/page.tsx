0519d93b7bded5e3e364d9a52a076bb9f06111a6
Mike Khaw <mikekhawnow@gmail.com>
2025-01-23 12:43:24 -0800

    Implement Edit and Get Server Actions and Implemention

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,14 +1,23 @@
+import { redirect } from "next/navigation";
 import React from "react";
+
+import { auth } from "~/auth";
 import QuestionForm from "@/components/forms/QuestionForm";
 
-const AskQuestion = () => {
+const AskQuestion = async () => {
+  // Get the user's authentication session to check if they're logged in
+  const session = await auth();
+
+  if (!session) return redirect("/sign-in");
+
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
       <div className="mt-9">
         <QuestionForm />
       </div>
-    </div>
+    </>
   );
 };
 
