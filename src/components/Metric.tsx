ae9226e5cbed5955451b946c437296fc34916897
Mike Khaw <mikekhawnow@gmail.com>
2025-01-28 09:26:05 -0800

    fixed user avatar in question card

--- src/components/Metric.tsx
+++ src/components/Metric.tsx
@@ -1,9 +1,11 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import UserAvatar from "@/components/UserAvatar";
 
 interface Props {
-  imgUrl: string | null;
+  imgUrl: string;
   alt: string;
   value: string | number;
   title: string;
@@ -11,6 +13,7 @@ interface Props {
   textStyles: string;
   imgStyles?: string;
   isAuthor?: boolean;
+  titleStyles?: string;
 }
 
 const Metric = ({
@@ -22,43 +25,35 @@ const Metric = ({
   textStyles,
   imgStyles,
   isAuthor,
+  titleStyles,
 }: Props) => {
-  const initials = alt
-    .split(" ")
-    .map((word: string) => word[0])
-    .join("")
-    .toUpperCase()
-    .slice(0, 2);
-
   const metricContent = (
     <>
-      {/* implemented custom avatar logic */}
-      {imgUrl ? (
+      {isAuthor ? (
+        <UserAvatar
+          id={href?.split("/").pop() || ""}
+          name={value.toString()}
+          imageUrl={imgUrl}
+          className="h-4 w-4"
+        />
+      ) : (
         <Image
           src={imgUrl}
-          width={16}
-          height={16}
+          width={20}
+          height={20}
           alt={alt}
           className={`rounded-full object-contain ${imgStyles}`}
         />
-      ) : (
-        <div
-          className={`primary-gradient flex h-4 w-4 items-center justify-center rounded-full ${imgStyles}`}
-        >
-          <span className="text-[8px] font-bold text-light-900">
-            {initials}
-          </span>
-        </div>
       )}
 
       <p className={`${textStyles} flex items-center gap-1`}>
         {value}
 
-        <span
-          className={`small-regular line-clamp-1 ${isAuthor ? "max-sm:hidden" : ""}`}
-        >
-          {title}
-        </span>
+        {title ? (
+          <span className={cn(`small-regular line-clamp-1`, titleStyles)}>
+            {title}
+          </span>
+        ) : null}
       </p>
     </>
   );
